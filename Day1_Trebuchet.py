bbe47b9ed621f2d7c278abd46b5bd5c5d2af1cf2
pweemaes-ro <zappamotel@quicknet.nl>
2023-12-01 14:17:07 +0100

    Minor code changes, better documentation.

--- Day1_Trebuchet.py
+++ Day1_Trebuchet.py
@@ -14,14 +14,18 @@ text_to_int = {"one": 1,
                "eight": 8,
                "nine": 9}
 
+# lookup table for digits, with corresponding values.
+digit_to_int = {c: int(c) for c in "123456789"}     # 0 is not an option...
+
 
 def get_digits(line: str, reverse: bool = False) -> tuple[int, int]:
 	"""Return tuple of two integers:
-	- first integer is the value of the first (or last) digit in line
-	- second integer is the value of the first (or last) digit or digit as
-	  string (whichever was found first).
-	If reverse == False, the values are the first found, elke the values are
-	the last found. Raises ValueError if no digits found in line."""
+	- first integer is the value of the first (or last) digit in the line when
+	  looking only for digits ('1', '2, ...,  '9')
+	- second integer is the value of the first (or last) digit in the line when
+	  also looking for digits as text ('one', 'two', ..., 'nine').
+	If reverse == False, the values are the first found in the line, else the
+	values are the last found in the line. Raises ValueError if no digits found in line."""
 	
 	if reverse:
 		indices = range(len(line) - 1, -1, -1)
@@ -32,10 +36,8 @@ def get_digits(line: str, reverse: bool = False) -> tuple[int, int]:
 	first_any: int | None = None
 	
 	for index in indices:
-		if first_digit is None and line[index] in digits:
-			first_digit = int(line[index])
-			first_any = first_any or first_digit
-			return first_digit, first_any
+		if first_digit := digit_to_int.get(line[index], first_digit):
+			return first_digit, first_any or first_digit
 
 		if first_any is None:
 			for length in (3, 4, 5):
@@ -46,8 +48,10 @@ def get_digits(line: str, reverse: bool = False) -> tuple[int, int]:
 
 
 def get_line_values(line: str) -> tuple[int, int]:
-	"""Return calibration values for the line. The first value only considers
-	digits, the second also considers digits as text."""
+	"""Return calibration values (integers) for the line. The first value is
+	the result of looking for digits ('1', '2', ..., '9'.) , the second value is the
+	result of looking for digits and 'digits as text' ('one', 'two', ..., 'nine').
+	"""
 	
 	first_digit, first_any = get_digits(line)
 	last_digit, last_any = get_digits(line, reverse=True)
